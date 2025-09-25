85bf9a8fbf8d688f163dcd74357e1220b91c0d97
selgas <s.elgas@techsmith.com>
2025-09-25 15:03:03 -0400

    Added function to draw arbitrary lines

--- Wolfensteino/screen.c
+++ Wolfensteino/screen.c
@@ -38,3 +38,79 @@ void Screen_DrawVerticalLineWithBorder( Screen_t* screen, u32 x, u32 y, u32 leng
    bufferPos += ( SCREEN_WIDTH * ( length - 1 ) );
    *bufferPos = borderColor;
 }
+
+void Screen_DrawLine( Screen_t* screen, u32 x1, u32 y1, u32 x2, u32 y2, u16 color )
+{
+   u32 x, y, prevY, w;
+   r32 adjacent, opposite, theta, tanTheta;
+   u16* bufferPos;
+   Bool_t flipY = False;
+
+   if ( x1 == x2 )
+   {
+      Screen_DrawVerticalLine( screen, x1, ( y1 < y2 ) ? y1 : y2, abs( (i32)y2 - (i32)y1 ), color );
+   }
+   else if ( y1 == y2 )
+   {
+      x = ( x1 < x2 ) ? x1 : x2;
+      w = (u32)abs( (i32)x2 - (i32)x1 );
+      bufferPos = screen->buffer + ( ( y1 * SCREEN_WIDTH ) + x );
+
+      for (; x < w; x++, bufferPos++ )
+      {
+         *bufferPos = color;
+      }
+   }
+   else
+   {
+      // ensure left-to-right drawing
+      if ( x2 < x1 ) { x = x2; x2 = x1; x1 = x; y = y2; y2 = y1; y1 = y; }
+
+      adjacent = (r32)x2 - (r32)x1;
+      opposite = (r32)y2 - (r32)y1;
+
+      if ( opposite < 0.0f )
+      {
+         opposite = -opposite;
+         flipY = True;
+      }
+
+      theta = atanf( opposite / adjacent );
+      tanTheta = tanf( theta );
+      bufferPos = screen->buffer;
+      prevY = y1;
+
+      if ( flipY )
+      {
+         for ( x = x1; x <= x2; x++ )
+         {
+            y = y1 - (u32)( ( x - x1 ) * tanTheta );
+            bufferPos[( y * SCREEN_WIDTH ) + x] = color;
+
+            while ( ( prevY - y ) > 1 )
+            {
+               prevY--;
+               bufferPos[( prevY * SCREEN_WIDTH ) + ( x - 1 )] = color;
+            }
+
+            prevY = y;
+         }
+      }
+      else
+      {
+         for ( x = x1; x <= x2; x++ )
+         {
+            y = y1 + (u32)( ( x - x1 ) * tanTheta );
+            bufferPos[( y * SCREEN_WIDTH ) + x] = color;
+
+            while ( ( y - prevY ) > 1 )
+            {
+               prevY++;
+               bufferPos[( prevY * SCREEN_WIDTH ) + ( x - 1 )] = color;
+            }
+
+            prevY = y;
+         }
+      }
+   }
+}
--- Wolfensteino/screen.h
+++ Wolfensteino/screen.h
@@ -31,6 +31,7 @@ extern "C" {
    void Screen_WipeColor( Screen_t* screen, u16 color );
    void Screen_DrawVerticalLine( Screen_t* screen, u32 x, u32 y, u32 length, u16 color );
    void Screen_DrawVerticalLineWithBorder( Screen_t* screen, u32 x, u32 y, u32 length, u16 lineColor, u16 borderColor );
+   void Screen_DrawLine( Screen_t* screen, u32 x1, u32 y1, u32 x2, u32 y2, u16 color );
 
    // platform-specific
    void Screen_RenderBuffer( Screen_t* screen );
